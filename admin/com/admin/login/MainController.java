52d0bf0ba892a517901dcda89324d34de5e45e17
nick <nick@comprame.com>
2016-03-08 20:59:49 +0800

    add admin

---
+++ admin/com/admin/login/MainController.java
@@ -0,0 +1,29 @@
+/**
+ *
+ * @Title MainController.java
+ * @Package com.admin.login
+ * @Description TODO
+ * @Date Feb 10, 2016 7:43:21 AM
+ * @version 1.0
+ */
+package com.admin.login;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName MainController
+ * @Description TODO
+ * @author Nick
+ * @version 1.0
+ * @Date Feb 10, 2016 7:43:21 AM
+ */
+@Controller
+@RequestMapping("/main")
+public class MainController {
+	
+	private Logger logger = Logger.getLogger(this.getClass());
+	
+	
+}
